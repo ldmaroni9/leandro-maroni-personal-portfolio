1d8c39022ce9728705888711e643f6988619e5cc
Leandro Daniel Maroni <41401967+ldmaroni9@users.noreply.github.com>
2024-03-20 11:20:11 -0600

    Update sql_demo.sql

--- sql_demo.sql
+++ sql_demo.sql
@@ -1,4 +1,27 @@
 --I will utilize this document to demonstrate my knowledge in several SQL functions 
+/*
+-Time Series Analysis:
+Write SQL scripts to perform time series analysis on historical data. This could include calculating moving averages, identifying trends, seasonality, and forecasting future values using techniques like exponential smoothing or ARIMA models.
 
+-Cohort Analysis:
+Develop SQL queries to conduct cohort analysis, where you analyze the behavior of groups of users/customers who share a common characteristic or experience within a defined time period. This could involve tracking metrics such as retention rates, average purchase frequency, and lifetime value over time.
+
+-Market Basket Analysis:
+Implement SQL scripts to perform market basket analysis on transactional data. Identify frequently co-occurring products in customer transactions using techniques like association rule mining (e.g., Apriori algorithm) to uncover patterns and inform cross-selling strategies.
+
+-Customer Segmentation:
+Write SQL queries to segment customers based on their attributes and behaviors. Use techniques like clustering (e.g., K-means clustering) or RFM analysis (recency, frequency, monetary value) to group customers into meaningful segments for targeted marketing and personalized recommendations.
+
+-Churn Prediction:
+Develop SQL scripts to predict customer churn using historical data. Build predictive models, such as logistic regression or decision trees, to identify factors that contribute to churn and predict the likelihood of individual customers churning in the future.
+
+-Text Analysis:
+Write SQL scripts to perform text analysis on unstructured data such as customer reviews or social media comments. Extract key insights using techniques like sentiment analysis, topic modeling (e.g., LDA), and keyword extraction to understand customer opinions and trends.
+-Advanced Aggregation and Window Functions:
+Scripts to calculate moving averages, rank data within partitions, and perform cumulative sums to derive insightful analytics from your datasets.
+
+-Performance Optimization
+-ETL Automation
+*/
 SELECT *
 FROM database
